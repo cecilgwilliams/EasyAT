d1ef9f8ce3212b14b43948c8643599f696d56ba3
Cecil Williams <cecil.g.williams@gmail.com>
2013-06-01 14:45:25 -0500

    cecil: rename scenario and switch back to firefox as default

--- src/main/groovy/com/cecilgwilliams/ExecuteTests.groovy
+++ src/main/groovy/com/cecilgwilliams/ExecuteTests.groovy
@@ -7,12 +7,12 @@ import com.cecilgwilliams.scenarios.DemoScenarioRunner
 try {
     def demoEnv = EnvironmentFactory.getDemo()
 
-    def chrome = BrowserFactory.getChrome()
-    chrome.manage().window().maximize();
-    new DemoScenarioRunner().run "scenarios/demo/posScenario", chrome, demoEnv
+//    def chrome = BrowserFactory.getChrome()
+//    chrome.manage().window().maximize();
+//    new DemoScenarioRunner().run "scenarios/demo/demoScenario", chrome, demoEnv
 
-//    def firefox = BrowserFactory.getFirefox()
-//    new DemoScenarioRunner().run "scenarios/demo/posScenario", firefox, demoEnv
+    def firefox = BrowserFactory.getFirefox()
+    new DemoScenarioRunner().run "scenarios/demo/demoScenario", firefox, demoEnv
 
 } catch (Exception e) {
     BrowserFactory.killAll()
